6d907559e78ebdc8bb4d4e21f90ebccf2dd2b47b
marsislav <mkostadinov87@gmail.com>
2022-11-10 18:50:58 +0200

    Version 0

--- footer.php
+++ footer.php
@@ -1,35 +1,7 @@
 <!-- FOOTER SECTION -->
 <footer>
-    <div class="container">
-      <div class="row">
-        <div class="col-12">
-          <h5>Highway Star 2.1</h5>
-          <h3>#</h3>
-          <ul class="contact-nav">
-            <li><a data-scroll href="#home">Home.</a></li>
-            <li><a data-scroll href="#about-us">About Us.</a></li>
-            <li><a data-scroll href="#portfolio">Portfolio.</a></li>
-            <li><a data-scroll href="#blog">Blog.</a></li>
-            <li><a data-scroll href="#contact-us">Contact.</a></li>
-          </ul>
-          <h6>© 2023. Marsislav.net</h6>
-          <ul class="social">
-            <li><a href="#"><i class="icofont-facebook"></i></a></li>
-            <li><a href="#"><i class="icofont-twitter"></i></a></li>
-            <li><a href="#"><i class="icofont-dribbble"></i></a></li>
-          </ul>
-        </div>
-      </div>
-    </div>
-  </footer>
-  <!-- Scripts -->
-  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
-  <script src="js/bootstrap.min.js"></script>
-  <script src="js/slick.min.js"></script>
-  <script src="js/smooth-scroll.min.js"></script>
-  <script src="js/main.js"></script>
-  <!-- Scripts Ends -->
+    
+</footer>
   <?php wp_footer();?>
 </body>
 </html>
